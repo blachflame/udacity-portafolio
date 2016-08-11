4072310d5770f83a1b821caee746ddcf6869d982
Jose Estevez <josefelixe@gmail.com>
2016-08-10 20:26:38 -0400

    mejoras

--- app/src/main/java/com/cl/fenix/portalfolio/PortafolioMainActivity.java
+++ app/src/main/java/com/cl/fenix/portalfolio/PortafolioMainActivity.java
@@ -1,6 +1,5 @@
 package com.cl.fenix.portalfolio;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,57 +13,16 @@ public class PortafolioMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portafolio_main);
-        final Context context = getApplicationContext();
-        Button button = (Button) findViewById(R.id.primeraOpcion);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Button boton = (Button)v;
-                CharSequence text = "This button will launch my " + boton.getText() + " App";
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        });
-
-         button = (Button) findViewById(R.id.segundaOpcion);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Button boton = (Button)v;
-                CharSequence text = "This button will launch my " + boton.getText() + " App";
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        });
-        button = (Button) findViewById(R.id.terceraOpcion);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Button boton = (Button)v;
-                CharSequence text = "This button will launch my " + boton.getText() + " App";
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        });
-        button = (Button) findViewById(R.id.cuartaOpcion);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Button boton = (Button)v;
-                CharSequence text = "This button will launch my " + boton.getText() + " App";
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        });
-        button = (Button) findViewById(R.id.quintaOpcion);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Button boton = (Button)v;
-                CharSequence text = "This button will launch my " + boton.getText() + " App";
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        });
 
 
     }
 
+    public void toastMessage(View v) {
+        Button boton = (Button)v;
+        CharSequence text = "This button will launch my " + boton.getText() + " App";
+        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+        toast.show();
+    }
 
 
 }
